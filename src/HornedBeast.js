c8fe8faa2e7a74c291c3e568b48f960b2fc63b42
Alan Chelko <achelko@gmail.com>
2022-08-18 01:00:36 +0200

    Set up of state and heart counter

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -5,17 +5,33 @@ import './hornedBeast.css';
 
 class HornedBeast extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      heartCount: 0
+    }
+  
+  
+  }
+
+  
+
+  increment = (e) => {
+
+    let nextState = {
+      heartCount: this.state.heartCount + 1
+    }
+    this.setState(nextState);
+  }
 
   render(){
     return (
       <div className="hornedBeast">
-       <h2>{this.props.title}</h2>
-       <p>{this.props.description}</p>
-       <img src={this.props.img_url} alt="Horned Beast" />
-        
+       <h2>{this.props.beasty.title}</h2>
+       <h3>Hearts = {this.state.heartCount}</h3>
+       <p>{this.props.beasty.description}</p>
+       <img src={this.props.beasty.image_url} alt={this.props.beasty.title} width="350" onClick={this.increment} />
       </div>
     );
    }
