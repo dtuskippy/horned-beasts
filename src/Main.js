7dd0319382470726f87f5761ffef97572965567c
Alan Chelko <achelko@gmail.com>
2022-08-18 00:59:59 +0200

    Set up of map function with imported data.json data

--- src/Main.js
+++ src/Main.js
@@ -3,20 +3,24 @@ import HornedBeast from './HornedBeast.js'
 import './main.css'
 import beastObjects from './data.json';
 
+
 class Main extends React.Component {
 
   // constructor(props) {
   //   super(props);
   // }
 
-  render(){
+  
+   render(){
     return (
-      // <main className="main">
-        <HornedBeast title={"UniWhal"} img_url={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"} description={beastObjects[0].description} />
+      <main className="main">
+        {
+        beastObjects.map( (beasty, index) =>
+        <HornedBeast beasty={beasty} />
+        )
+        }
         
-        
-        
-      // </main>
+      </main>
     );
    }
 
